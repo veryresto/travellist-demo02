ef94d9358bc6aeb43ed0b039a8ad74460b5b03d9
veryresto <veryresto@gmail.com>
2025-02-26 04:12:38 +0000

    add migration places

---
+++ database/migrations/2025_02_26_041116_create_places.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlaces extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('places', function (Blueprint $table) {
+            $table->integer('id')->autoIncrement();
+            $table->string('name', 255);
+            $table->boolean('visited');
+            $table->primary('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('places');
+    }
+}
